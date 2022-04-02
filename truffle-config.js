868975a7e81bf9a23ed1a2348045b905a2d1446a
sejigner <sejigner@gmail.com>
2022-04-02 10:59:49 +0900

    Set up initially

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const NETWORK_ID = "1001";
 const GASLIMIT = "20000000";
 const URL = `https://api.baobab.klaytn.net:8651`;
 const PRIVATE_KEY =
-  "";
+  "0x95f6f59e8b098f7ff90f773f20bdf83bf73c9d06d4db3d91500747e139e2bded";
 
 module.exports = {
   networks: {
@@ -13,11 +13,11 @@ module.exports = {
       network_id: "*", // Match any network id
     },
 
-    klaytn: {
-      provider: new HDWalletProvider(PRIVATE_KEY, URL),
-      network_id: NETWORK_ID,
-      gas: GASLIMIT,
-      gasPrice: null,
-    },
+    // klaytn: {
+    //   provider: new HDWalletProvider(PRIVATE_KEY, URL),
+    //   network_id: NETWORK_ID,
+    //   gas: GASLIMIT,
+    //   gasPrice: null,
+    // },
   },
 };
